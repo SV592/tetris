15ea018ef13a336cb14994cb208fc3ddb2beb021
Shaquille Pearson <shaquillepearson9@gmail.com>
2025-07-04 23:03:10 -0400

    wall kicks , transition function, randomizer function and rotate function

--- script.js
+++ script.js
@@ -56,3 +56,48 @@ const TETROMINOS = [
         name: "Z",
     },
 ];
+
+
+// SRS Kick Data (Wall Kick Data)
+const WALL_KICKS_JLSTZ = {
+    "0_1": [[0, 0], [-1, 0], [-1, 1], [0, -2], [-1, -2]],
+    "1_2": [[0, 0], [1, 0], [1, -1], [0, 2], [1, 2]],
+    "2_3": [[0, 0], [1, 0], [1, 1], [0, -2], [1, -2]],
+    "3_0": [[0, 0], [-1, 0], [-1, -1], [0, 2], [-1, 2]],
+    "0_3": [[0, 0], [1, 0], [1, 1], [0, -2], [1, -2]],
+    "3_2": [[0, 0], [-1, 0], [-1, -1], [0, 2], [-1, 2]],
+    "2_1": [[0, 0], [-1, 0], [-1, 1], [0, -2], [-1, -2]],
+    "1_0": [[0, 0], [1, 0], [1, -1], [0, 2], [1, 2]],
+};
+
+const WALL_KICKS_I = {
+    "0_1": [[0, 0], [-2, 0], [1, 0], [-2, 1], [1, -2]],
+    "1_2": [[0, 0], [-1, 0], [2, 0], [-1, -2], [2, 1]],
+    "2_3": [[0, 0], [2, 0], [-1, 0], [2, -1], [-1, 2]],
+    "3_0": [[0, 0], [1, 0], [-2, 0], [1, 2], [-2, -1]],
+    "0_3": [[0, 0], [-1, 0], [2, 0], [-1, -2], [2, 1]],
+    "3_2": [[0, 0], [2, 0], [-1, 0], [2, -1], [-1, 2]],
+    "2_1": [[0, 0], [1, 0], [-2, 0], [1, 2], [-2, -1]],
+    "1_0": [[0, 0], [-2, 0], [1, 0], [-2, 1], [1, -2]],
+};
+
+// Helper function to get kick data for a specific transition
+function getKickData(pieceName, originalState, nextState) {
+    const key = `${originalState}_${nextState}`;
+    if (pieceName === "I") {
+        return WALL_KICKS_I[key] || [];
+    } else {
+        return WALL_KICKS_JLSTZ[key] || [];
+    }
+}
+
+// Generate a random tetromino with a starting position and initial rotation state
+function randomTetromino() {
+    const i = Math.floor(Math.random() * TETROMINOS.length);
+    return { ...TETROMINOS[i], pos: { x: 3, y: 0 }, rotationState: 0 };
+}
+
+// Rotate a tetromino shape (clockwise)
+function rotate(shape) {
+    return shape[0].map((_, i) => shape.map((row) => row[i]).reverse());
+}
